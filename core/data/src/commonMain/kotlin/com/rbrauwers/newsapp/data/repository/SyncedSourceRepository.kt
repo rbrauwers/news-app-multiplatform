d43c4f74d86a230f5bac770b3da49fb4a2e7046b
rbrauwers <rodrigo.brauwers@dock.tech>
2023-12-22 09:42:29 -0300

    Execute data operations in IO dispatcher

--- core/data/src/commonMain/kotlin/com/rbrauwers/newsapp/data/repository/SyncedSourceRepository.kt
+++ core/data/src/commonMain/kotlin/com/rbrauwers/newsapp/data/repository/SyncedSourceRepository.kt
@@ -7,8 +7,11 @@ import com.rbrauwers.newsapp.database.model.toExternalModel
 import com.rbrauwers.newsapp.model.NewsSource
 import com.rbrauwers.newsapp.network.NetworkDataSource
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 
@@ -18,23 +21,27 @@ class SyncedSourceRepository(
 ) : SourceRepository {
 
     override fun getSources(): Flow<List<NewsSource>?> {
-        return dao.getSources().map { it.map { source -> source.toExternalModel() } }
+        return dao.getSources()
+            .map { it.map { source -> source.toExternalModel() } }
+            .flowOn(Dispatchers.IO)
     }
 
     override suspend fun sync() {
-        runCatching {
-            val response = networkDataSource.getSources()
+        withContext(Dispatchers.IO) {
+            runCatching {
+                val response = networkDataSource.getSources()
 
-            // Saves data in local store regardless even if coroutine context was cancelled
-            withContext(NonCancellable) {
-                if (response.status.isOk()) {
-                    dao.upsertSources(response.sources.map { it.toEntity() })
+                // Saves data in local store regardless even if coroutine context was cancelled
+                withContext(NonCancellable) {
+                    if (response.status.isOk()) {
+                        dao.upsertSources(response.sources.map { it.toEntity() })
+                    }
                 }
+            }.onFailure {
+                // Do not suppress coroutine cancellations
+                if (it is CancellationException) throw it
+                println("SyncedSourceRepository::sync failure $it")
             }
-        }.onFailure {
-            // Do not suppress coroutine cancellations
-            if (it is CancellationException) throw it
-            println("SyncedSourceRepository::sync failure $it")
         }
     }
 
